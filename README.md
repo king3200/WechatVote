3eb59ddceb160f39e60558dc696e9b72b9229359
TidusWang <tidusff2000@163.com>
2018-08-21 09:55:31 +0800

    remove voter editable in admin page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 restful投票项目
+django 2.0.7
+djangorestframework 3.8.2
+djangorestframework-jwt 1.11.0
--- Vote/admin.py
+++ Vote/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from Vote.models import VotingItem, Voter, VoteEvent
+from Vote.models import VotingItem, VoteEvent
 
-admin.site.register(Voter)
 admin.site.register(VoteEvent)
 
 
